b43de590c8f05ac56b9f8e5b17d09fac92b041be
Agente <Agente@SALC0393.focusservices.com>
2022-09-17 16:24:03 -0600

    feat: Session 5

--- Session 4 - JavaScript/functions.js
+++ Session 4 - JavaScript/functions.js
@@ -1,23 +1,24 @@
+const errormessage = document.getElementById("errormessage");
 function CrearTabla() {
-  const body = document.body,
+  const content = document.getElementById("content"),
     tbl = document.createElement("table");
-  tbl.style.width = "100px";
-  tbl.style.border = "1px solid black";
-  const header = ["Id", "Nombre", "Apellido"];
+  tbl.style.width = "50%";
+  const header = ["Id", "Nombre", "Apellido", "Fecha de nacimiento"];
 
   const tr = tbl.insertRow();
   var ctrl = document.createElement("input");
   ctrl.type = "checkbox";
   tbl.id = "tbl";
-  for (let j = 0; j < 3; j++) {
+  tbl.style.cellspacing = 0;
+  tbl.style.cellpading = 0;
+  for (let j = 0; j < 4; j++) {
     const td = tr.insertCell();
     td.appendChild(document.createTextNode(header[j]));
-    td.style.border = "1px solid black";
   }
-  tbl.rows[0].insertCell(3);
-  tbl.rows[0].cells[3].appendChild(ctrl);
+  tbl.rows[0].insertCell(4);
+  tbl.rows[0].cells[4].appendChild(ctrl);
 
-  body.appendChild(tbl);
+  content.appendChild(tbl);
 }
 
 function Agregar() {
@@ -33,23 +34,32 @@ function Aceptar() {
   var v1 = document.frmCaptura.id.value;
   var v2 = document.frmCaptura.nom.value;
   var v3 = document.frmCaptura.ape.value;
+  var v4 = document.frmCaptura.bdate.value;
+  let date = new Date(v4);
+  errormessage.innerHTML = "";
+  if (v1 !== "" && v2 !== "" && v3 !== "" && v4 !== "") {
+    var tbl = document.getElementById("tbl");
+    var numFilas = tbl.rows.length;
+    tbl.insertRow(numFilas);
+    var ctrl = document.createElement("input");
+    ctrl.type = "checkbox";
+    ctrl.className = "check";
 
-  var tbl = document.getElementById("tbl");
-  var numFilas = tbl.rows.length;
-  tbl.insertRow(numFilas);
-  var ctrl = document.createElement("input");
-  ctrl.type = "checkbox";
+    tbl.rows[numFilas].insertCell(0);
+    tbl.rows[numFilas].cells[0].textContent = v1;
+    tbl.rows[numFilas].insertCell(1);
+    tbl.rows[numFilas].cells[1].textContent = v2;
+    tbl.rows[numFilas].insertCell(2);
+    tbl.rows[numFilas].cells[2].textContent = v3;
+    tbl.rows[numFilas].insertCell(3);
+    tbl.rows[numFilas].cells[3].textContent = date.toLocaleDateString();
+    tbl.rows[numFilas].insertCell(4);
+    tbl.rows[numFilas].cells[4].appendChild(ctrl);
 
-  tbl.rows[numFilas].insertCell(0);
-  tbl.rows[numFilas].cells[0].textContent = v1;
-  tbl.rows[numFilas].insertCell(1);
-  tbl.rows[numFilas].cells[1].textContent = v2;
-  tbl.rows[numFilas].insertCell(2);
-  tbl.rows[numFilas].cells[2].textContent = v3;
-  tbl.rows[numFilas].insertCell(3);
-  tbl.rows[numFilas].cells[3].appendChild(ctrl);
-
-  Cancelar();
+    Cancelar();
+  } else {
+    errormessage.innerHTML = "Todos los campos son obligatorios";
+  }
 }
 
 function Cancelar() {
@@ -61,27 +71,17 @@ function Cancelar() {
   document.frmCaptura.id.value = "";
   document.frmCaptura.nom.value = "";
   document.frmCaptura.ape.value = "";
+  document.frmCaptura.bdate.value = "";
+  errormessage.innerHTML = "";
 }
 
 function Eliminar() {
-  /*var tbl = document.getElementById("tbl");
-  var numFilas = tbl.rows.length;
-  var lstEliminar = new Array();
-  for (var x = 1; x < numFilas; x++) {
-    if (tbl.rows[x].cells[3].childNodes[0].checked);
-    lstEliminar.push(x);
-  }
-  if (lstEliminar.length == 0) {
-    alert("No hay filas seleccionadas");
+  var chEliminar = document.querySelectorAll(".check");
+  if (chEliminar.length > 0) {
+    chEliminar.forEach((box) => {
+      if (box.checked) box.closest("tr").remove();
+    });
   } else {
-    for (var i = lstEliminar.length - 1; i >= 0; i--) {
-      tbl.deleteRow(lstEliminar[i]);
-    }
-    tbl.rows[0].cells[3].childNodes[0].checked = false;
-  }*/
-  var tbl = document.getElementById("tbl");
-  var numFilas = tbl.rows.length;
-  for (var x = 1; x <= numFilas; x++) {
-    if(tbl.rows[x].cells[3].childNodes[0].checked) tbl.deleteRow(x);
+    console.log("Debe seleccionar un elemento para eliminar");
   }
 }
